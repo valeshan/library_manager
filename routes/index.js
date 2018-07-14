cb03102af56de6c321adb4113f9033a5fc288da7
valeshan <valeshan@gmail.com>
2018-07-14 23:00:25 +1000

    added book|patron files&function
    -updated book detail form
    -added new patron form
    -added patron and book table
    -added get requests for all books/patrons/loans
    -added filter for overdue loans

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.get('/', function(req, res, next) {
 
 
 
-//GET All Books
+//GET ALL BOOKS
 router.get('/all_books', function(req,res,err){
   Books.findAll().then(function(books){
       res.render("all_books", {books:books})
@@ -37,19 +37,41 @@ router.get('/overdue_books', function(req, res, next) {
     Loans.findAll({where: {
                     return_by:{
                             [Op.lt]: date
-    }}}).then(function(loans){
-      res.render('overdue_books', {loans:loans});
+                              }
+                  }
+    }).then(function(loans){
+      res.render('overdue_books', {loans:loans})
     })
   });
 
+
+//GET CHECKED BOOKS
 router.get('/checked_books', function(req, res, next) {
   res.render('checked_books');
 });
 
+
+//ADD NEW BOOK
 router.get('/new_book', function(req, res, next) {
   res.render('new_book');
 });
 
+//GET BOOK DETAIL
+router.get('/all_books/:id', function(req,res,err){
+  Books.findById(req.params.id).then(function(bookDetail){
+      Loans.findAll({
+        where: {
+          book_id : req.params.id
+        },
+        include:[
+          { model: Books},
+          { model: Patrons}
+        ]
+      }).then(function(loanedBook){
+        res.render("book_detail", {bookDetail, loanedBook})
+      })
+  })
+});
 
 
 
@@ -77,7 +99,44 @@ router.get('/overdue_loans', function(req, res, next) {
     })
   });
 
+//GET CHECKED OUT LOANS
+router.get('/checked_loans', function(req, res, next) {
+    Loans.findAll({where: {
+                    loaned_on:{
+                            [Op.ne]: null
+                          },
+                    returned_on:{
+                            [Op.eq]: null
+                    }
+  }}).then(function(loans){
+      res.render('all_loans', {loans:loans});
+    })
+  });
 
+
+
+  //************ PATRONS *************//
+
+
+//GET ALL PATRONS
+router.get('/all_patrons', function(req,res,next){
+  Patrons.findAll().then(function(patrons){
+      res.render("all_patrons", {patrons:patrons})
+  })
+});
+
+//POST NEW PATRON
+router.post('/new_patron', function(req, res, next){
+  Patrons.create(req.body).then(function(patron){
+    res.redirect('/all_patrons')
+  })
+})
+
+
+//ADD NEW PATRON
+router.get('/new_patron', function(req, res, next) {
+  res.render('new_patron', {patron: Patrons.build()})
+});
 
 
 
